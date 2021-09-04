49e206e61c8884be5247aa7990c65c1e753ff28a
yoshiro1412 <josiasrojasramires@gmail.com>
2021-09-04 16:37:17 -0400

    connection to the database

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,9 +1,19 @@
+const { username, password } = require('./credentials.json');
+const pgp = require('pg-promise')();
+const db = pgp(`postgres://${username}:${password}@localhost:5432/ecommerce`);
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
-    res.send('Hello world');
+app.get('/', async (req,res) => {
+    db.one('SELECT * FROM product')
+    .then(data => {
+        res.send(data);
+    })
+    .catch(error => {
+        console.error('Error: '+ error);
+    });
 })
 
 app.listen(port, () => {
